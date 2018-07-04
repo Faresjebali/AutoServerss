6ab2f6b51aade3e2fcd96ce4f77ea4f9ceed5601
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-07-05 00:57:55 +0200

    Update and rename cat.js to stats2.js

---
+++ commands/stats2.js
@@ -0,0 +1,55 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+   let embed_fields = [{
+                name: "System info:",
+                value: `${process.platform}-${process.arch} with ${process.release.name} version ${process.version.slice(1)}`,
+                inline: true
+            },
+            {
+                name: "Process info: PID",
+                value: `${process.pid}`,
+                inline: true
+            },
+            {
+                name: "Process memory usage:",
+                value: `${Math.ceil(process.memoryUsage().heapTotal / 1000000)} MB`,
+                inline: true
+            },
+            {
+                name: "System memory usage:",
+                value: `${Math.ceil((os.totalmem() - os.freemem()) / 1000000)} of ${Math.ceil(os.totalmem() / 1000000)} MB`,
+                inline: true
+            },
+            {
+                name: "Uptime bot:",
+                value: `:clock12: ${upTimeOutput}`,
+                inline: true
+            },
+            {
+                name: "Uptime computer:",
+                value: `:clock1230: ${upTimeOutput2}`,
+                inline: true
+            },{
+                name: 'Lib',
+                value: `**Discord.js**`
+            }
+        ];
+
+        msg.channel.send({
+            embed: {
+                author: {
+                    name: msg.author.username,
+                    icon_url: msg.author.avatarUrl,
+                    url:'http://google.fr'
+                },
+                color: 0x00FF00,
+                fields: embed_fields
+            }
+        });
+}
+
+message.channel.send(embed_fields);
+
+}
